43eba64d93c8a83b1d7305f88ef313bcebf662f9
Golan Ziv <zivg121@gmail.com>
2025-11-18 10:28:18 +0200

    exercise 0 - FIXED: removed 4 arrays in main, declared vars in loop scopes, removed non main functions to keep only one loop and determine order while reading the input.

--- exercise-0/array_series.c
+++ exercise-0/array_series.c
@@ -9,129 +9,52 @@
 * Author: Golan Ziv
 -------------------------------------------------------*/
 #include <stdio.h>
-#define SIZE 15
-#define MODES_AMNT 3 // not including messy
-
-// Indecies of modes : -1 = messy, 0 = increasing, 1 = decreasing, 2 = constant
-enum Mode {
-    MESSY = -1,
-    INC, 
-    DEC, 
-    CON
-};
-
-
-/*------------------------------------------------------
-* Function Name - determine_array_order 
-*
-* Function Purpose - to return the state of an array (one of the 4 order modes) 
-*
-* Parameters –  [IN nums - an array of integers] 
-*               [IN length - the length of the array]
-*		        
-* Return Values - the state of the array: -1 = messy, 0 = increasing, 1 = decreasing, 2 = constant
-*
-* Author - Golan Ziv
--------------------------------------------------------*/
-int determine_array_order(int nums[SIZE], int length){
-    int modes[MODES_AMNT] = {1, 1, 1}; // set all to true, will change later.
-    int prev = nums[0], state = -1;
-    // state starts of as messed up, if later we see its something else, it will change
-    
-    for (int i = 1; i < length; i++){
-        if(prev > nums[i]){
-            modes[INC] = 0; // CANT BE INCREASING, we set increasing mode to 0, indicating it's not an increasing array
-            modes [CON] = 0; // CANT BE CONSTANT, we set constant mode to 0, indicating it's not a constant array
-        }
-        else if(prev < nums[i]){
-            modes[DEC] = 0; // CANT BE DECREASING, we set decreasing mode to 0, indicating it's not an decreasing array
-            modes[CON] = 0; // CANT BE CONSTANT, we set constant mode to 0, indicating it's not a constant array
-        }
-        prev = nums[i];
-    }
-
-    // if some mode is still true, we set the state to it.
-    if(modes[INC]) state = INC; 
-    if(modes[DEC]) state = DEC;
-    if(modes[CON]) state = CON;
-
-    //  NOTE: Increasing and decreasing modes will never be true at the same time.
-
-    /*  NOTE: There can be a situation where the constant mode is true and the increasing / decreasing is true
-        The way I set it up, a constant array is also increasing, but the loop on the modes ensures it
-        it checks constant at the end, this way it will be more accurate. */
-
-    return state;
-}
-
-/*------------------------------------------------------
-* Function Name - print_array_order
-*
-* Function Purpose - to print the state of the array order 
-*
-* Parameters –  [IN nums - an array of integers] 
-*               [IN length - the length of the array]
-
-* Return Values - void
-*
-* Author - Golan Ziv
--------------------------------------------------------*/
-void print_array_order(int nums[SIZE], int length){
-    int state = determine_array_order(nums, length);
-
-    switch(state){
-        case MESSY:
-            printf("The array is messed up\n");
-            break;
-        case INC:
-            printf("The array is increasing\n");
-            break;
-        case DEC:
-            printf("The array is decreasing\n");
-            break;
-        case CON:
-            printf("The array is constant\n");
-            break;
-    }
-
-}
+#define SIZE 4
 
 /*------------------------------------------------------
 * Function Name - main 
 *
-* Function Purpose - creates 4 demonstrate the function "print_array_order" 
+* Function Purpose - takes 15 integers, and determine their order
 *
 * Return Values - 0 for success
 *
 * Author - Golan Ziv
 -------------------------------------------------------*/
+
+// Indecies of modes : 0 = increasing, 1 = decreasing, 2 = constant
+enum Mode {
+    INC, 
+    DEC, 
+    CON
+};
+
 int main() {
-    /* 
-    we create 4 arrays of size 15.
-    each of them will be ordered in one of the four ways 
-    (increasing, decreasing, constant, messy)
-    then the function will be used on all 4 of them to print their order
-    */
-    int inc_arr[SIZE] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int dec_arr[SIZE] = {120,116,114,63,50,7,-5,-18,-20,-21,-55,-60,-82,-92,-100};
-    int con_arr[SIZE] = {17,17,17,17,17,17,17,17,17,17,17,17,17,17,17};
-    int mess_arr[SIZE] = {1,2,3,4,120,6,7,8,9,10,11,-2200,13,14,15};
-    int user_arr[SIZE] = {0};
-    int curr;
-    print_array_order(inc_arr, SIZE);
-    print_array_order(dec_arr, SIZE);
-    print_array_order(con_arr, SIZE);
-    print_array_order(mess_arr, SIZE);
-    
-    
-    printf("Enter a sequence of 15 integers: ");
-    for(int i = 0; i < SIZE; i++){
-        scanf(" %d", &curr);
-        user_arr[i] = curr;
+    int modes[] = {1, 1, 1}; 
+
+    for(int i = 0, curr, prev; i < SIZE; i++){
+        scanf("%d", &curr);
+
+        if(i == 0){
+            prev = curr;
+            continue;
+        }
+
+        if(curr > prev){
+            modes[CON] = 0;
+            modes[DEC] = 0;
+        }
+        else if (prev > curr){
+            modes[INC] = 0;
+            modes[CON] = 0;
+        }
+
+        prev = curr;
     }
 
-    printf("\nResult for your array: ");
-    print_array_order(user_arr, SIZE);
+    if(modes[CON]) printf("The array is constant\n");
+    else if(modes[INC]) printf("The array is increasing\n");
+    else if(modes[DEC]) printf("The array is decreasing\n");
+    else printf("The array is messed up\n");
 
     return 0;
 }
